9e093f3fe343a57d5a87d91ee48b0717812d92ce
Jack-Web-PM <jack.chou24@student.cart.org>
2025-02-05 14:52:46 -0800

    js updadte

--- main.js
+++ main.js
@@ -4,4 +4,30 @@ document.querySelector("button").addEventListener("click", () => {
 
 const CertificateImg = document.querySelector("img");
 
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+
+
+  myButton.addEventListener("click", () => {
+    setUserName();
+  });
+  
+
+
+
+
+// CertificateImg.querySelector(">").addEventListener("click", () => {
+//     const mySrc = CertificateImg.getAttribute("src");
+//     if 
+
+// });
