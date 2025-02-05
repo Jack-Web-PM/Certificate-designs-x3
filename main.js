5dca6865c4eb9b3667edcd560f7678037f6b4b89
Jack-Web-PM <jack.chou24@student.cart.org>
2025-02-05 14:20:11 -0800

    css update

--- main.js
+++ main.js
@@ -4,11 +4,4 @@ document.querySelector("button").addEventListener("click", () => {
 
 const CertificateImg = document.querySelector("img");
 
-CertificateImg.querySelector(">").addEventListener("click", () => {
-    const mySrc = CertificateImg.getAttribute("src");
-    if (mySrc === "images/firefox-icon.png") {
-        myImage.setAttribute("src", "images/firefox2.png");
-      } else {
-        myImage.setAttribute("src", "images/firefox-icon.png");
-      }
-});
+
