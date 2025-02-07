dc3ab1387723ef552fbc329c249f5a14cf5d5f04
Jack-Web-PM <jack.chou24@student.cart.org>
2025-02-07 15:12:47 -0800

    update
    function on top would be action first, if putting a querySelector("button") will calling the first button.

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-document.querySelector("button").addEventListener("click", () => {
-    alert("what do you want!"); 
-});
+// document.querySelector("button").addEventListener("click", () => {
+//     alert("Please enter your name!"); 
+// });
 
-const CertificateImg = document.querySelector("img");
 
-let myButton = document.querySelector("button");
+//variable
+// let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h2");
+let myImage = document.querySelector("img");
+let mySecButton = document.querySelector("#try");
+let myUserName = document.querySelector("#Name");
 
 
-
-
-function setUserName() {
+function setUserName() { //set user name function
     const myName = prompt("Please enter your name.");
     if (!myName) {
       setUserName();
@@ -21,16 +22,33 @@ function setUserName() {
   }
 
 
-  myButton.addEventListener("click", () => {
+  myUserName.addEventListener("click", () => {//function called
     setUserName();
   });
-  
+
+  mySecButton.addEventListener("click", () => {//image changer
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "image/computer science cropped.png") {
+      myImage.setAttribute("src", "image/spiderman point each other cropped.png");
+    } else {
+      myImage.setAttribute("src", "image/women yelling at cat cropped.png");
+    } 
+  });
 
 
 
+//test
+// let plusBtn = document.querySelector("#next");
 
-// CertificateImg.querySelector(">").addEventListener("click", () => {
-//     const mySrc = CertificateImg.getAttribute("src");
-//     if 
-
+// plusBtn.addEventListener("click", () => {
+//   const myImg = myImage.getAttribute("Img");
+//   if (myImg === "image/computer science.jpg") {
+//     myImage.setAttribute("Img", "image/download.jpg");
+//   } if (myImg === "image/download.jpg") {
+//     myImage.setAttribute("Img", "image/women yelling at cat.jpg");
+//   } if (myImg === "image/women yelling at cat.jpg") {
+//     myImage.setAttribute("Img", "image/computer science.jpg");
+//   }
+//   console.log(plusBtn);
 // });
+
